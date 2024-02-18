0968150d23318d7b00cd0446458cf80dd95e4ee3
caleb0109 <70354203+caleb0109@users.noreply.github.com>
2024-02-18 13:30:30 -0500

    set up switch in client.html and worked on some of js files

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+
+const htmlHandler = require('./htmlResponse.js');
+const jsonHandler = require('./jsonResponse.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const handleHead = () => {
+
+};
+
+const handlePost = () => {
+
+};
+
+const handleGet = () => {
+
+}
+
+const onRequest = () => {
+
+};
